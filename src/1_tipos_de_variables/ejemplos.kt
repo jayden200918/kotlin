621d6287bc4749ab305dbea7a06b4295082d5e3f
jayden200918 <jaydencastrosaenz@gmail.com>
2025-03-24 11:12:52 -0600

    agrege

---
+++ src/1_tipos_de_variables/ejemplos.kt
@@ -0,0 +1,29 @@
+package `1_tipos_de_variables`
+
+fun main() {
+    // variable inmutable de tipo entero
+    val Edad: Int
+    Edad = 48
+    // variable inmutable de tipo decimal
+    val Sueldo: Float
+    Sueldo = 1200.55f //siempre lleva una "f" al final del valor
+    //variable inmutable de tipo decimal
+    val Total: Double
+    Total = 70000.24
+    //variable inmutable de tipo alfanumerico
+    val Titulo: String
+    Titulo = "sistema de ventas"
+    //Variable mutable, las variables mutables enpiezan con "var"
+    var Mes: Int
+    Mes = 1
+    //Algunas lineas mas de codigo
+    Mes = 2
+    // variable tipo Char, almacena un unico caracter
+    var continua: Char = 's'
+    // Variable de tipo Boolean, true o false
+    var fin : Boolean = false
+    print("jayden")
+
+    println("castro")
+    println("saenz")
+    }
---
+++ src/1_tipos_de_variables/proyecto2.kt
@@ -0,0 +1,22 @@
+fun main() {
+    /*
+     Crear un programa que defina dos variables inmutables de tipo Int. Luego definir una
+     tercer variable mutable que almacene la suma de las dos primeras variables y las
+     muestre. Seguidamente almacenar en la variable el producto de las dos primeras
+     variables y mostrar el resultado.
+     */
+    // variable inmutable de tipo Int
+    val varia1: Int
+    varia1= 2
+    //variable mutable de tipo int
+    val varia2: Int
+    varia2= 5
+    var suma: Int
+    suma= varia1 + varia2
+    // suma de la operacion
+    println(suma)
+    var producto: Int
+    producto= varia1 * varia2
+    print(producto )
+
+}
