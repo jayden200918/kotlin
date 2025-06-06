fcb0635c15fee4afab39e75dd0bfb330cf4afbb0
jayden200918 <jaydencastrosaenz@gmail.com>
2025-06-06 09:41:10 -0600

    sistema de calificaciones

--- src/proyecto_kotlin/Sistemadecalificaciones.kt
+++ src/proyecto_kotlin/Sistemadecalificaciones.kt
@@ -1,32 +1,59 @@
 package proyecto_kotlin
 
-fun registrarnota(nombre: String) : Int {
-    var nota : Int = 0
-        do {
-            print("ingrese una nota del 0 al 100 ${nombre[0]}")
-            var nota = readln().toInt()
+fun registrarnota() : Int {
+     var nota = 0
+    do {
+            print("ingrese una nota del 0 al 100:")
+            nota = readln().toInt()
             while (nota > 100 || nota < 0) {
-                print("la nota ingresada es invalida , vuelve a intentarlo")
+                print("la nota ingresada es invalida , vuelve a intentarlo:")
             }
-        }while(nota in 0 ..100)
+        }while(nota !in 0 ..100)
         return (nota)
 }
 fun main() {
-    val nota = readln().toInt()
+    val nombres = arrayOfNulls<String>(5)
+    val nota = IntArray(5)
     var estado = String
+    var reprobados = 0
     var notaalta = 0
-    var notabaja = 0
-    val suma  =
+    var notabaja = 100
+    var suma = 0
+    var aprobados = 0
+
     for (i in 0..4) {
-        print("ingrese el nombre del estudiante")
+        print("ingrese el nombre del estudiante:")
         val nombre = readln().toString()
-    when {
-        nota in 90..100 -> println("Excelente")
-        nota in 80..89 -> println("Buena")
-        nota in 70..79 -> println("Aprobado")
-        nota < 70 -> println("Reprobada")
+        nombres[i] = nombre
+        nota[i]= registrarnota()
+        suma += nota[i]
+        println(nota[i])
+        when {
+            nota[i] in 90..100 -> {
+                println("Excelente")
+                aprobados++}
+            nota[i] in 80..89 -> {
+                println("Bueno")
+                aprobados++ }
+            nota[i] in 70..79 ->{
+                println("Aprobada")
+                aprobados++ }
+            else -> {
+                println("Reprobado")
+                reprobados++}
+        }
+
+        if (notaalta < nota[i]) {
+            notaalta = nota[i]
+        }
+        if (notabaja > nota[i]){
+            notabaja = nota[i]
     }
-        val promedio = nota + nota + nota + nota + nota/5
-        if
-    }
+}
+   val promedio = suma/nota.size
+    println("los que aprobaron son: $aprobados")
+    println("los que reprobaron son: $reprobados")
+    println("la nota ingresada mas baja es : $notabaja")
+    println("la nota ingresada mas alta es : $notaalta")
+    println("En promedio las notas son de: $promedio")
 }
