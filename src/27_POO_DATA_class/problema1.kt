570999cf71f3d6042bcfaf318026ebc151b4d9e7
jayden200918 <jaydencastrosaenz@gmail.com>
2025-09-01 08:34:40 -0600

    Cotidiano

--- src/27_POO_DATA_class/problema1.kt
+++ src/27_POO_DATA_class/problema1.kt
@@ -1,34 +1,44 @@
-package `27_POO_DATA_class  11`
-//Un pequeño cine quiere llevar el registro de las butacas que ya fueron vendidas para cada función. Cada butaca solo necesita guardar
-// su número (1…N) y si está ocupada (true/false)
-//Tarea para los estudiantes
-//Declarar un data class llamado Butaca con dos propiedades:
-//numero: Int
-//ocupada: Boolean
-//En main() construir una lista con 5 butacas (números 1..5) inicialmente libres (ocupada = false).
-//Simular la venta de 2 butacas: elegir al azar dos números distintos y marcarlas como ocupadas (ocupada = true).
-//	Pista: usar random y un simple bucle para buscar el número.
-//Imprimir la lista completa de butacas usando println() y observar cómo se ve la salida por defecto del data class.
-//Volver a imprimir, pero ahora recorriendo la lista con un for y desestructurando las propiedades (número, ocupada) para mostrar un mensaje amigable:
-//“Butaca 3 → libre” o “Butaca 3 → ocupada”.
-//Hacer una copia de la lista antes de vender y comparar ambas listas con == para verificar que son distintas referencias.
-// Sobreescribir toString() para que devuelva “Butaca #X” seguido de “(libre)” o “(ocupada)”.//
+package `27_POO_DATA_class`
 
-import kotlin.random.Random
+data class Butaca(val num: Int, var ocupada: Boolean)
 
-class dado(var num1 : Int){
-    fun lanzar(){
-        num1 =  ((Math.random()*6)+1).toInt()
-        println(num1)
+
+fun main() {
+    val butaca1 = Butaca(1, false)
+    val butaca2 = Butaca(2, false)
+    val butaca3 = Butaca(3, false)
+    val butaca4 = Butaca(4, false)
+    val butaca5 = Butaca(5, false)
+
+    val butacas = listOf(butaca1, butaca2, butaca3, butaca4, butaca5)
+
+    val copiaAntesDeVender = listOf(
+        Butaca(1,butaca1.ocupada),
+        Butaca(2, butaca2.ocupada),
+        Butaca(3, butaca3.ocupada),
+        Butaca(4, butaca4.ocupada),
+        Butaca(5, butaca5.ocupada)
+    )
+
+    var num1 = ((Math.random()*5)+1).toInt()
+    var num2 = ((Math.random()*5)+1).toInt()
+    while (num2 == num1) {
+        num2 = ((Math.random()*5)+1).toInt()
     }
 
+    for (butaca in butacas) {
+        if (butaca.num == num1 || butaca.num == num2) {
+            butaca.ocupada = true
+        }
+    }
+
+    println("Lista de butacas")
+    println(butacas)
+
+    println("Estado de cada butaca:")
+    for (butaca in butacas) {
+        val estado = if (butaca.ocupada) "ocupada" else "libre"
+        println(" $butaca esta = $estado")
+    }
 
 }
-data class butaca (var butacas : Boolean, var num: Int)
-    fun main() {
-        var butaca1 = butaca (true, 1)
-        var butaca2 = butaca(false , 2)
-        var butaca3 = butaca(false ,3)
-        var butaca4 = butaca(true, 4)
-        var butaca5 = butaca(true , 5 )
-    }
